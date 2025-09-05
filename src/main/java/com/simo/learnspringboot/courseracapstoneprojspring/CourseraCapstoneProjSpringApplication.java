11f9db4e3a384bbf5b96ab3a7da18f6d5b1c14ec
Admiral-Simo <admiralsimo16@gmail.com>
2025-09-05 10:23:34 +0100

    Initial commit

---
+++ src/main/java/com/simo/learnspringboot/courseracapstoneprojspring/CourseraCapstoneProjSpringApplication.java
@@ -0,0 +1,13 @@
+package com.simo.learnspringboot.courseracapstoneprojspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseraCapstoneProjSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseraCapstoneProjSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simo/learnspringboot/courseracapstoneprojspring/CourseraCapstoneProjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simo.learnspringboot.courseracapstoneprojspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseraCapstoneProjSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
